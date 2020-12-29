9f18cf6059a94eb1e23c4ca597a0ae96c810f610
Princerin <erosnick@gmail.com>
2020-12-29 21:43:26 +0800

    Add basic error handling.

--- Hazel/src/Platform/Windows/Win32Window.cpp
+++ Hazel/src/Platform/Windows/Win32Window.cpp
@@ -15,6 +15,27 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) glClearError();\
+	x;\
+ASSERT(GLLogCall(__FUNCTION__, __FILE__, __LINE__))
+
+static void glClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		HAZEL_CORE_ERROR("[OpenGL Error]({0}, {1}, {2}, line:{3})\n", error, function, file, line);
+		return false;
+	}
+
+	return true;
+}
+
 struct ShaderSource
 {
 	std::string vertexSource;
@@ -179,7 +200,7 @@ namespace Hazel
 	{
 		glBindVertexArray(vao[1]);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
 
 		glfwSwapBuffers(window);
 	}
